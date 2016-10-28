05d4d92c0069a87ef51764e20d01a780377fc143
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-10-27 22:53:49 -0700

    Add post_spec model tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,4 @@
-Factory.define :user do |f|
-  f.sequence(:email) { |n| "user@gmail.com" }
-  f.password "shhhhhh!"
-end
+# Factory.define :user do |f|
+#   f.sequence(:email) { |n| "user@gmail.com" }
+#   f.password "shhhhhh!"
+# end
