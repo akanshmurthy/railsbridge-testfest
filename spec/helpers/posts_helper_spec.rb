94b3667af4281a2d966d7f86abab2c69e4c4aee2
Akansh Murthy <ak45m16@gmail.com>
2016-11-02 00:08:12 -0700

    add 1st set of Twilio tests

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -7,5 +7,16 @@ describe PostsHelper do
       user.save
       helper.find_user_email(user.id).should eq("user@gmail.com")
     end
+
+    it "initiates a Twilio client" do
+      client = helper.initiate_twilio_client
+      expect(client).to be_an_instance_of(Twilio::REST::Client)
+    end
+
+    it "sends a text" do
+      @number = 1234567890 #enter your phone number here
+      response = JSON.parse(helper.sent_text_to_user(@number))
+      expect(response).to be_an_instance_of(Hash)
+    end
   end
 end
